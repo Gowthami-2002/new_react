84077bb261bef63825a8a73cd982b2381a3bc7fa
Gowthami-2002 <vallurivenkaiahppm@gmail.com>
2025-10-27 22:13:57 +0530

    Four-Tags

---
+++ Four_Tags/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+function App() {
+  return (
+    <>
+      <h1>Hello welcome back React</h1>
+      <marquee>Gowthami</marquee>
+      <p>This is paragraph tag</p>
+      <h2>This is Second Heading tag</h2>
+    </>
+  )
+}
+
+export default App
